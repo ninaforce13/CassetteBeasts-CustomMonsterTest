8f88ade77e7ce92299ad09aea71d6a0d81828bdd
NCrafters <68625676+ninaforce13@users.noreply.github.com>
2023-07-17 11:34:11 -0400

    Update mod_load.gd
    Added take_over_path for Frankie extensions

--- Custom Monster/mod_load.gd
+++ Custom Monster/mod_load.gd
@@ -4,10 +4,15 @@ extends ContentInfo
 var monster = preload("res://mods/Custom Monster/customform1.tres")
 var tapecollection_ext: Resource = preload("TapeCollections_Ext.gd")
 var party_ext: Resource = preload("Party_Ext.gd")
+var choose_frankie_ext: Resource = preload("ChooseFrankieTape_Ext.gd")
+var frankie_tape_config_ext: Resource = preload("FrankieTapeConfig_Ext.gd")
+
 func _init():
 	
 	tapecollection_ext.take_over_path("res://global/save_state/TapeCollection.gd")
 	party_ext.take_over_path("res://global/save_state/Party.gd")
+	choose_frankie_ext.take_over_path("res://cutscenes/sidequests/frankie/ChooseFrankieTape.gd")
+	frankie_tape_config_ext.take_over_path("res://world/quest_scenes/FrankieTapeConfig.gd")
 	
 	load_monster(monster)
 	
